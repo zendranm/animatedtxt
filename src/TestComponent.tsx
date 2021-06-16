b7b82ecb91db80f8968359253c98a74bdb7a0075
zendranm <zendran.m@gmail.com>
2021-06-16 17:10:13 +0200

    Initial setup

---
+++ src/TestComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const TestComponent = (props: any) => {
+  return (
+    <div>
+      <h4>Hello There!</h4>
+    </div>
+  );
+};
+
+export default TestComponent;
