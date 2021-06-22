69365b769afef8e570c197d81d093115b5ab4ee4
zendranm <zendran.m@gmail.com>
2021-06-22 18:16:37 +0200

    Basic styled components setup

--- src/components/characters/CharH.tsx
+++ src/components/characters/CharH.tsx
@@ -1,16 +1,41 @@
 import React from "react";
-import "../../styles/CharH.scss";
+import styled from 'styled-components'
+
+const Svg = styled.svg`
+stroke: #000000;
+stroke-width: 36;
+`
+
+type LineProp = {
+  delay: number;
+};
+
+const Line = styled.line<LineProp>`
+stroke-dasharray: 100%;
+stroke-dashoffset: 100%;
+animation: dash linear;
+animation-duration: 5s; //Animation length (without delay)
+animation-fill-mode: forwards; //Animated object stays instead of disappearing
+animation-delay: ${(props: LineProp) => props.delay}s;
+
+@keyframes dash {
+  from {
+    stroke-dashoffset: 100%;
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+`
 
 const CharH = () => {
   return (
-    <div className="character">
-      <svg viewBox="0 0 590 140">
-        <g>
-          <line className="path" id="h1" x1="20" y1="0" x2="20" y2="140" />
-          <line className="path" id="h2" x1="90" y1="0" x2="90" y2="140" />
-          <line className="path" id="h3" x1="20" y1="70" x2="90" y2="70" />
-        </g>
-      </svg>
+    <div>
+      <Svg viewBox="0 0 590 140">
+        <Line delay={0.5} x1="20" y1="0" x2="20" y2="140" />
+        <Line delay={1.0} x1="90" y1="0" x2="90" y2="140" />
+        <Line delay={1.5} x1="20" y1="70" x2="90" y2="70" />
+      </Svg>
     </div>
   );
 };
